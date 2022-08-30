7a36fab83b74005073495c64426612b42dbe9331
Michael Bemmerl <mail@mx-server.de>
2022-08-30 23:13:27 +0200

    Buildscript with compressing and signing the release files.

--- build-release.bat
+++ build-release.bat
@@ -1,11 +1,30 @@
 @echo off
+setlocal
 
-echo Building for win-x64...
+set prefix=ID3-AutoGen
+set rar="%ProgramW6432%\WinRAR\winrar.exe"
+
+echo Enter new version number in format X.Y.Z:
+set /p version=""
+
+echo Building...
+echo ...for win-x64
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained false -o dist\win-x64> nul
-echo done.
-echo Building for linux-x64...
+echo ...for linux-x64
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained false -o dist\linux-x64 > nul
-echo done.
-echo Building for osx-x64...
+echo ...for osx-x64
 dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true --self-contained false -o dist\osx-x64 > nul
+
+cd dist
+
+echo Compressing...
+%rar% a -ep %prefix%-%version%-win.zip win-x64\*.*
+%rar% a -ep %prefix%-%version%-linux.zip linux-x64\*.*
+%rar% a -ep %prefix%-%version%-osx.zip osx-x64\*.*
+
+echo Signing...
+gpg -a --detach-sign %prefix%-%version%-win.zip
+gpg -a --detach-sign %prefix%-%version%-linux.zip
+gpg -a --detach-sign %prefix%-%version%-osx.zip
+
 echo done.
